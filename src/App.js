0d491f3bdcaeb2fe41c850c84aae3790cfb4c172
Chris Burke <cburke@onvia.com>
2017-09-22 18:36:23 -0700

    removing unused code

--- src/App.js
+++ src/App.js
@@ -16,12 +16,6 @@ class App extends Component {
 
   render() {
     return (
-      // <div className="container">
-      //   <Router history={browserHistory}>
-      //     <Route path="/" component={Login}/>
-      //     <Route path="/save_the_date" component={SaveTheDate}/>
-      //   </Router>
-      // </div>
       <BrowserRouter>
         <Main />
       </BrowserRouter>
